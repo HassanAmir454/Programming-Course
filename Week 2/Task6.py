4e6e94f866c0f3de3d435a6f86b861cdca6313ea
Your Name <Hassan.Amir@student.lab.fi>
2025-09-27 16:00:18 +0500

    task6w2update

--- Week 2/Task6.py
+++ Week 2/Task6.py
@@ -1,11 +1,10 @@
-print("Program starting.")
+print("Program starting.\n")
 
 color = input("Insert a hex color: ")
 
-print("Colors")
-
-print(f"- Red  {color[1:3]}")
-print(f"- Green  {color[3:5]}")
-print(f"- Blue  {color[5:7]}")
+print("\nColors")
+print(f"- Red {color[1:3]}")
+print(f"- Green {color[3:5]}")
+print(f"- Blue {color[5:7]}\n")
 
 print("Program ending.")
